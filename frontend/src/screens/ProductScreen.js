fba59cd7d64e7e489eddcb7188186648ce1d257c
Prakash Mahat <shadyprakash8@gmail.com>
2021-07-24 21:05:14 +0545

    qty select and add to cart button

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 /** @format */
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {
@@ -10,13 +10,16 @@ import {
 	Card,
 	Button,
 	ListGroupItem,
+	Form,
 } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import {listProductDetails} from '../actions/productActions';
 
-const ProductScreen = ({match}) => {
+const ProductScreen = ({history, match}) => {
+	const [qty, setQty] = useState(0);
+
 	const dispatch = useDispatch();
 
 	const productDetails = useSelector((state) => state.productDetails);
@@ -25,6 +28,10 @@ const ProductScreen = ({match}) => {
 	useEffect(() => {
 		dispatch(listProductDetails(match.params.id));
 	}, [dispatch, match]);
+
+	const addToCartHandler = () => {
+		history.push(`/cart/${match.params.id}?qty=${qty}`);
+	};
 
 	return (
 		<>
@@ -78,8 +85,29 @@ const ProductScreen = ({match}) => {
 									</Row>
 								</ListGroup.Item>
 
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty</Col>
+											<Col>
+												<Form.Control
+													as='select'
+													value={qty}
+													onChange={(e) => setQty(e.target.value)}>
+													{[...Array(product.countInStock).keys()].map((x) => (
+														<option key={x + 1} value={x + 1}>
+															{x + 1}
+														</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
+
 								<ListGroup.Item>
 									<Button
+										onClick={addToCartHandler}
 										className='btn-block'
 										type='button'
 										disabled={product.countInStock === 0}>
---
+++ frontend/src/screens/cartScreen.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react';
+
+const cartScreen = () => {
+	return <div>cart</div>;
+};
+
+export default cartScreen;
