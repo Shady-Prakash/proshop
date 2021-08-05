45956097966a1905eb5ecba0cdbf1c82942f063a
Prakash Mahat <shadyprakash8@gmail.com>
2021-08-05 16:21:34 +0545

    mark order as delivered

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,6 +6,7 @@ import {
 	addOrderItems,
 	getOrderById,
 	updateOrderToPaid,
+	updateOrderToDelivered,
 	getMyOrders,
 	getOrders,
 } from '../controllers/orderController.js';
@@ -15,4 +16,5 @@ router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
 export default router;
